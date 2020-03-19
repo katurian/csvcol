35d8082f99a614a39bbc5a337c0d6250f0e43730
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-03-19 19:22:58 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,12 +1,33 @@
 import os       
 import csv
+from zipfile import ZipFile
 
-def extractColumns(inputCSV, outputCSV, cols):
+def addColumns(inputCSV, outputCSV, cols):
     cols = sorted(cols, reverse=True)
-    with open(inputCSV, "r") as input:
-        reader = csv.reader(input)
+    with open(inputCSV, "r") as inp:
+        reader = csv.reader(inp)
         with open(outputCSV, "w", newline='') as output:
             writer = csv.writer(output)
             for row in reader:
                 writer.writerow([row[n] for n in cols])
      os.remove(inputCSV)
+        
+def removeColumns(inputCSV, outputCSV, cols):   
+    with open(inputCSV, "r") as inp:
+        reader = csv.reader(inp)
+        with open(outputCSV, "w", newline='') as output:
+            writer = csv.writer(output)
+            for row in reader:
+                for index in cols:
+                    del row[index]
+                writer.writerow(row)
+    os.remove(inputCSV)
+    
+def fromZipExtractCSVs(inputZip):   
+    with ZipFile(inputZip, 'r') as zipObj:
+       files = zipObj.namelist()
+       for fileName in files:
+           if fileName.endswith('.csv'):
+               zipObj.extract(fileName)
+    os.remove(inputZip)
+  
