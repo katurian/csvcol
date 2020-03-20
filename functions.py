9d215cd29a0b582f29e250a380da8b7c97f25056
Kate Kulinski <35544223+katurian@users.noreply.github.com>
2020-03-19 20:18:06 -0400

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
-import os       
+import os
 import csv
 from zipfile import ZipFile
+
 
 def addColumns(inputCSV, outputCSV, cols):
     cols = sorted(cols, reverse=True)
@@ -11,8 +12,10 @@ def addColumns(inputCSV, outputCSV, cols):
             for row in reader:
                 writer.writerow([row[n] for n in cols])
     os.remove(inputCSV)
-        
-def removeColumns(inputCSV, outputCSV, cols):   
+
+
+def removeColumns(inputCSV, outputCSV, cols):
+    cols = sorted(cols, reverse=True)
     with open(inputCSV, 'r') as ip:
         reader = csv.reader(ip)
         with open(outputCSV, 'w', newline='') as op:
@@ -21,13 +24,14 @@ def removeColumns(inputCSV, outputCSV, cols):
                 for index in cols:
                     del row[index]
                 writer.writerow(row)
-    os.remove(inputCSV)
-    
-def fromZipExtractCSVs(inputZip):   
-    with ZipFile(inputZip, 'r') as zipObj:
-       files = zipObj.namelist()
-       for fileName in files:
-           if fileName.endswith('.csv'):
-               zipObj.extract(fileName)
     os.remove(inputZip)
-  
+
+
+def fromZipExtractCSVs(inputZip):
+    with ZipFile(inputZip, 'r') as zipObj:
+        files = zipObj.namelist()
+        for fileName in files:
+            if fileName.endswith('.csv'):
+                zipObj.extract(fileName)
+    os.remove(inputZip)
+    
